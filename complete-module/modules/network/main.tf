e8f0fabae5730a2b746775552df2c32b883ed811
Tuxtarve <Tuxtarve@example.com>
2026-03-27 17:07:06 +0900

    feat: 인터넷 게이트웨이 및 퍼블릭 라우팅 설정 추가

--- complete-module/modules/network/main.tf
+++ complete-module/modules/network/main.tf
@@ -35,3 +35,28 @@ resource "aws_subnet" "private" {
     "kubernetes.io/role/internal-elb"  = "1"
   }
 }
+
+# 4. 인터넷 게이트웨이 (VPC의 대문)
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "${var.project_name}-igw" }
+}
+
+# 5. 퍼블릭 라우팅 테이블 (인터넷으로 가는 길)
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0" # 모든 트래픽을 IGW로 보냄
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = { Name = "${var.project_name}-public-rt" }
+}
+
+# 6. 퍼블릭 서브넷과 라우팅 테이블 연결 (Association)
+resource "aws_route_table_association" "public_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
